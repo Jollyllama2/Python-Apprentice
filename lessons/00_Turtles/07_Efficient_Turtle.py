e6303bdf188430b9923ccbc37f8885ed5a6af6da
Jollyllama2 <139662407+Jollyllama2@users.noreply.github.com>
2024-08-18 19:58:23 +0000

    qwert

--- lessons/00_Turtles/07_Efficient_Turtle.py
+++ lessons/00_Turtles/07_Efficient_Turtle.py
@@ -17,18 +17,20 @@ tina.speed(2)                           # Make the turtle move as fast, but not 
 
 def draw_polygon(sides):
 
-    angle = ... # Calculate angle from number of sides
+    angle = 360/sides # Calculate angle from number of sides
     
-    for i in range(...):                 # Loop through the number of sides
-        ...                              # Move tina forward by the forward distance
-        ...                              # Turn tina left by the left turn
+    for i in range(sides):                 # Loop through the number of sides
+        turtle.forward(40)                              # Move tina forward by the forward distance
+        turtle.left(angle)                             # Turn tina left by the left turn
 
 
-draw_polygon(...)                        # Draw a square
+draw_polygon()
+    
+              # Draw a square
 
 ...                                      # Move tina to another spot on the screen
 
-draw_polygon(...)                        # Draw a pentagon
+draw_polygon()                        # Draw a pentagon
 
 ...                                      # Move tina to another spot on the screen
 
